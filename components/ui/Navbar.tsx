a9756da02e5ad39f46dbdd6870da0ba08ac00b45
Dawson Gilbert <falln.beatz@gmail.com>
2026-07-06 22:09:30 -0400

    implement database setup and much more

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -9,6 +9,7 @@ import { SignInButton, SignUpButton, Show, UserButton, useClerk, useUser } from 
 const navItems = [
     {label: "Library", href: "/"},
     {label: "Add New", href: "/books/new"},
+    {Label: "Pricing", href: "/pricing"}
 ]
 
 const Navbar = () => {
--- lib/zod.ts
+++ lib/zod.ts
@@ -1,16 +1,14 @@
 import { z } from 'zod';
-import { ACCEPTED_PDF_TYPES, MAX_FILE_SIZE } from './constants';
+import {ACCEPTED_IMAGE_TYPES, ACCEPTED_PDF_TYPES, MAX_FILE_SIZE, MAX_IMAGE_SIZE} from './constants';
 
 export const UploadSchema = z.object({
     title: z.string().min(1, 'Title is required').max(100),
     author: z.string().min(1, 'Author Name is required').max(100),
-    voice: z.string().min(1, 'Please select a voice'),
-    pdf: z
-        .custom<File>((val) => val instanceof File, 'PDF file is required')
+    persona: z.string().min(1, 'Please select a voice'),
+    pdfFile: z.instanceof(File, { message: 'PDF file is required'})
         .refine((file) => file.size <= MAX_FILE_SIZE, `File size must be less than 50MB`)
         .refine((file) => ACCEPTED_PDF_TYPES.includes(file.type), 'Only PDF files are accepted'),
-    cover: z
-        .custom<File>((val) => val instanceof File)
-        .optional()
-        .refine((file) => !file || file.size <= 10 * 1024 * 1024, `Image size must be less than 10MB`),
+    coverImage: z.instanceof(File).optional()
+        .refine((file) => !file || file.size <= MAX_IMAGE_SIZE, `Cover image size must be less than 10MB`)
+        .refine((file) => !file || ACCEPTED_IMAGE_TYPES.includes(file.type), 'Only .jpg, .jpeg, .png and .webp files are accepted')
 });
